3edbdf458a79f1e4ec992204f5550d21ba5cde26
Jacob Tepperman <jtepp@icloud.com>
2020-06-27 22:59:11 -0400

    rm callback

--- functions/app.js
+++ functions/app.js
@@ -1,11 +1,12 @@
-exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers':
-                'Origin, X-Requested-With, Content-Type, Accept',
-        },
-        body: JSON.stringify({ msg: "Hello", other: 3456, bd: event.body })
-    })
+exports.handler = function (event, context) {
+    // callback(null, {
+    //     statusCode: 200,
+    //     headers: {
+    //         'Access-Control-Allow-Origin': '*',
+    //         'Access-Control-Allow-Headers':
+    //             'Origin, X-Requested-With, Content-Type, Accept',
+    //     },
+    //     body: JSON.stringify({ msg: "Hello", other: 3456, bd: event.body })
+    // })
+    return "this"
 }
--- script.js
+++ script.js
@@ -8,8 +8,14 @@ async function app() {
     await fetch(`https://ecstatic-blackwell-7a1538.netlify.app/.netlify/functions/app`, {
         method: 'POST',
         mode: 'no-cors',
+        headers: {
+            'content-type': 'text/plain;charset=UTF-8'
+        },
+        body: 'here'
     })
-        .then(res => res.json())
+        .then(res => res)
 
         .then(d => console.log(d))
 }
+
+app()
