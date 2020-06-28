2a241a6d6c3d3a9d60dc366d3914f5c4896b8fcb
Jacob Tepperman <jtepp@icloud.com>
2020-06-27 22:18:22 -0400

    testing fetch

--- functions/app.js
+++ functions/app.js
@@ -1,6 +1,11 @@
 exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
-        body: "Hello"
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':
+                'Origin, X-Requested-With, Content-Type, Accept',
+        },
+        body: { "msg": "Hello", "other": "poop" }
     })
 }
--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var str = "a"
+// var str = "a"
 
-fetch(`https://ecstatic-blackwell-7a1538.netlify.app/.netlify/functions/app`)
+fetch(`https://ecstatic-blackwell-7a1538.netlify.app/.netlify/functions/app`, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+        'content-type': 'text/plain'
+    },
+    // body: "bye"
+})
     .then(res => { console.log(res); res.json() })
     .then(data => { console.log(data); str = data; })
 
 
-document.body.innerHTML += str
+// document.body.innerHTML += str
