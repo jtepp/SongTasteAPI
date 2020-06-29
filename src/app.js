661e64796ecf294599d1ef1d8fdf92ccebf0dd10
Jacob Tepperman <jtepp@icloud.com>
2020-06-29 17:32:47 -0400

    send raw

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 exports.handler = function (event, context, callback) {
-
-    var output = "Hey there"
-    console.log(event.body)
+    var data = {}
+    const s = event.body;
+    console.log(s)
+    // for (let i = 1; i < s.length - 1; i++) {
+    //     const e = s[i].split('\\')
+    //     // console.log(e)
+    //     // data[e[2].replace('"', '')] = Number(e[3].replace('": ', '').replace(',', ''))
+    // }
+    // console.log(data)
     callback(null, {
-
         statusCode: 200,
         headers: {
+
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers':
                 'Origin, X-Requested-With, Content-Type, Accept',
         },
-        body: event.body[0]//JSON.stringify({ msg: "Hello", other: 3456, bd: event.body })
+        body: s//JSON.stringify({ msg: "Hello", other: 3456, bd: event.body })
     })
 
 }
