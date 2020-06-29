f5f116dfe6bf855606d838ff8881b357cffd8006
Jacob Tepperman <jtepp@icloud.com>
2020-06-29 12:59:26 -0400

    using express

--- functions/app.js
+++ functions/app.js
@@ -1,20 +1,33 @@
-exports.handler = function (event, context, callback) {
-    var brain;
-    var output = "Hey there"
-    try { var brain = require("brain.js"); }
-    catch (e) { var output = 'false ' + e }
+// exports.handler = function (event, context, callback) {
+//     var brain;
+//     var output = "Hey there"
+//     try { var brain = require("brain.js"); }
+//     catch (e) { var output = 'false ' + e }
 
 
 
 
-    callback(null, {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers':
-                'Origin, X-Requested-With, Content-Type, Accept',
-        },
-        body: JSON.stringify({ out: output })
+//     callback(null, {
+//         statusCode: 200,
+//         headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Headers':
+//                 'Origin, X-Requested-With, Content-Type, Accept',
+//         },
+//         body: JSON.stringify({ out: output })
+//     })
+
+// }
+
+const express = require('express')
+const serverless = require('serverless-http')
+
+const app = express()
+const router = express.Router()
+router.get('/', (req, res) => {
+    res.json({
+        'hello': 'hi'
     })
-
-}
+})
+app.use('/.netlify/functions/app', router)
+module.exports.handler = serverless(app)
