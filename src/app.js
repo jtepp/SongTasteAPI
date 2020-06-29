853a6c85be7a3c5ea7e02471b18b95a9eb72368c
Jacob Tepperman <jtepp@icloud.com>
2020-06-29 13:31:23 -0400

    all

--- src/app.js
+++ src/app.js
@@ -23,11 +23,11 @@ const express = require('express')
 const serverless = require('serverless-http')
 
 const app = express()
-const router = express.Router()
-router.post('/', (req, res) => {
+app.all('/', function (req, res, next) {
     res.send(
         `'hello': 'hi'`
     )
+    next()
 })
 app.use('/.netlify/functions/app', router)
 module.exports.handler = serverless(app)
