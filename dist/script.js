ea1d1d4c53bb3b1b7a02a652fe318ee1bbcd5a23
Jacob Tepperman <jtepp@icloud.com>
2020-06-29 22:09:50 -0400

    fetch songs

--- dist/script.js
+++ dist/script.js
@@ -4,9 +4,13 @@ var run = []
 
 
 
-
 async function test() {
-    fetch(`https://songtaste.netlify.app/.netlify/functions/app`, {
+    fetch("https://songtaste.netlify.app/songs.json", {
+    })
+        .then(response => response.json())
+        .then(json => { inp = json; console.log(json) });
+
+    await fetch(`https://songtaste.netlify.app/.netlify/functions/app`, {
         method: 'POST',
         'content-type': 'application/json',
         body: JSON.stringify(inp)
