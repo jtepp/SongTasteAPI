11fc01a5fec2c8588e450ad22dbfa1607276e04f
Jacob Tepperman <jtepp@icloud.com>
2020-06-30 13:21:30 -0400

    added fonts, box to html

--- dist/script.js
+++ dist/script.js
@@ -1,26 +1,31 @@
 var inp = {};
 var train = []
 var run = []
+const banner = document.getElementById('banner')
+const spacer = document.getElementById("spacer")
 
+{// banner.style.height = "210px";
+    // spacer.style.height = "210px";
+    // window.onscroll = () => {
 
+    //     if (document.documentElement.scrollTop > 50) {
+    //         banner.style.height = "70px";
+    //         spacer.style.height = "120px";
+    //     } else {
+    //         banner.style.height = "210px";
+    //         spacer.style.height = "260px";
+    //     }
+    // }
+    // window.scrollTo(0, 0)}
 
-async function test() {
-    await fetch(`https://songtaste.netlify.app/.netlify/functions/app`, {
-        method: 'POST',
-        'content-type': 'application/json',
-        body: JSON.stringify(inp)
-        // mode: 'no-cors'
-    }).then(res => res.json())
-        .then(d => console.log(d))
-}
-
-// test()
-
-window.onscroll = () => {
-    const banner = document.getElementById('banner')
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        banner.style.height = "70px";
-    } else {
-        banner.style.height = "210px";
+    async function test() {
+        await fetch(`https://songtaste.netlify.app/.netlify/functions/app`, {
+            method: 'POST',
+            'content-type': 'application/json',
+            body: JSON.stringify(inp)
+            // mode: 'no-cors'
+        }).then(res => res.json())
+            .then(d => console.log(d))
     }
 }
+// test()
