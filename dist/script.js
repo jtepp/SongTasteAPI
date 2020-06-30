ae33d6be41505f3ba36c2076efeab57d80fc28d9
Jacob Tepperman <jtepp@icloud.com>
2020-06-29 20:18:43 -0400

    WORKING synaptic

--- dist/script.js
+++ dist/script.js
@@ -1,161 +1,179 @@
 var inp = {
-    "train":
-        [
-            //Break My Face: 
-            {
-                "input": {
-                    "acousticness": 0.00321,
-                    "danceability": 0.451,
-                    "duration_ms": 226336,
-                    "energy": 0.661,
-                    "instrumentalness": 0,
-                    "liveness": 0.108,
-                    "speechiness": 0.0976,
-                    "tempo": 180.133,
-                    "valence": 0.444
-                }, "output": { "like": true }
-            },
-            // M.O.N.E.Y.:
-            {
-                "input": {
-                    "acousticness": 0.0934,
-                    "danceability": 0.423,
-                    "duration_ms": 216563,
-                    "energy": 0.661,
-                    "instrumentalness": 0.000129,
-                    "liveness": 0.125,
-                    "speechiness": 0.07,
-                    "tempo": 114.26,
-                    "valence": 0.621,
-                    "like": true
-                }, "output": { "like": true }
-            },
-            // Old Fashioned: 
-            {
-                "input": {
-                    "acousticness": 0.0133,
-                    "danceability": 0.52,
-                    "duration_ms": 166733,
-                    "energy": 0.878,
-                    "instrumentalness": 0,
-                    "liveness": 0.0433,
-                    "speechiness": 0.0616,
-                    "tempo": 168.033,
-                    "valence": 0.783
-                }, "output": { "like": false }
-            },
-            // Bad Blood: 
-            {
-                "input": {
-                    "acousticness": 0.0157,
-                    "danceability": 0.663,
-                    "duration_ms": 212613,
-                    "energy": 0.777,
-                    "instrumentalness": 0.00829,
-                    "liveness": 0.0755,
-                    "speechiness": 0.0375,
-                    "tempo": 143.003,
-                    "valence": 0.94
-                }, "output": { "like": true }
-            },
-            // Blinding Lights: 
-            {
-                "input": {
-                    "acousticness": 0.00146,
-                    "danceability": 0.514,
-                    "duration_ms": 200040,
-                    "energy": 0.73,
-                    "instrumentalness": 0.0000954,
-                    "liveness": 0.0897,
-                    "speechiness": 0.0598,
-                    "tempo": 171.005,
-                    "valence": 0.334
-                }, "output": { "like": true }
-            },
-            // I Don't Care: 
-            {
-                "input": {
-                    "acousticness": 0.0912,
-                    "danceability": 0.798,
-                    "duration_ms": 219947,
-                    "energy": 0.675,
-                    "instrumentalness": 0,
-                    "liveness": 0.0894,
-                    "speechiness": 0.0442,
-                    "tempo": 101.956,
-                    "valence": 0.842
-                }, "output": { "like": false }
-            },
-            //Victorious: 
-            {
-                "input": {
-                    "acousticness": 0.00205,
-                    "danceability": 0.57,
-                    "duration_ms": 178600,
-                    "energy": 0.865,
-                    "instrumentalness": 0,
-                    "liveness": 0.0404,
-                    "speechiness": 0.0472,
-                    "tempo": 110.01,
-                    "valence": 0.691
-                }, "output": { "like": false }
-            },
-            // Say Amen (Saturday Night): 
-            {
-                "input": {
-                    "acousticness": 0.167,
-                    "danceability": 0.491,
-                    "duration_ms": 189187,
-                    "energy": 0.867,
-                    "instrumentalness": 0,
-                    "liveness": 0.458,
-                    "speechiness": 0.103,
-                    "tempo": 167.965,
-                    "valence": 0.602
-                }, "output": { "like": true }
-            },
-            //nothing revealed
-            {
-                "input": {
-                    "acousticness": 0.634,
-                    "danceability": 0.554,
-                    "duration_ms": 218497,
-                    "energy": 0.776,
-                    "instrumentalness": 0.00277,
-                    "liveness": 0.115,
-                    "speechiness": 0.0722,
-                    "tempo": 169.052,
-                    "valence": 0.324
-                }, "output": { "like": true }
-            },
-            {
-                //6 Summers
-                "input": {
-                    "acousticness": 0.0774,
-                    "danceability": 0.649,
-                    "duration_ms": 282987,
-                    "energy": 0.587,
-                    "instrumentalness": 0,
-                    "liveness": 0.377,
-                    "speechiness": 0.316,
-                    "tempo": 92.871,
-                    "valence": 0.513
-
-                },
-                "output": { "like": false }
-
-            }
-        ], "run": {
-            "acousticness": 0.0912,
-            "danceability": 0.798,
-            "duration_ms": 219947,
-            "energy": 0.675,
-            "instrumentalness": 0,
-            "liveness": 0.0894,
-            "speechiness": 0.0442,
-            "tempo": 101.956,
-            "valence": 0.842
+    "train": [
+        {
+            "input": [
+                0.00321,
+                0.451,
+                226336,
+                0.661,
+                0,
+                0.108,
+                0.0976,
+                180.133,
+                0.444
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.0934,
+                0.423,
+                216563,
+                0.661,
+                0.000129,
+                0.125,
+                0.07,
+                114.26,
+                0.621,
+                1
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.0133,
+                0.52,
+                166733,
+                0.878,
+                0,
+                0.0433,
+                0.0616,
+                168.033,
+                0.783
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.0157,
+                0.663,
+                212613,
+                0.777,
+                0.00829,
+                0.0755,
+                0.0375,
+                143.003,
+                0.94
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.00146,
+                0.514,
+                200040,
+                0.73,
+                0.0000954,
+                0.0897,
+                0.0598,
+                171.005,
+                0.334
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.0912,
+                0.798,
+                219947,
+                0.675,
+                0,
+                0.0894,
+                0.0442,
+                101.956,
+                0.842
+            ],
+            "output": [
+                0
+            ]
+        },
+        {
+            "input": [
+                0.00205,
+                0.57,
+                178600,
+                0.865,
+                0,
+                0.0404,
+                0.0472,
+                110.01,
+                0.691
+            ],
+            "output": [
+                0
+            ]
+        },
+        {
+            "input": [
+                0.167,
+                0.491,
+                189187,
+                0.867,
+                0,
+                0.458,
+                0.103,
+                167.965,
+                0.602
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.634,
+                0.554,
+                218497,
+                0.776,
+                0.00277,
+                0.115,
+                0.0722,
+                169.052,
+                0.324
+            ],
+            "output": [
+                1
+            ]
+        },
+        {
+            "input": [
+                0.0774,
+                0.649,
+                282987,
+                0.587,
+                0,
+                0.377,
+                0.316,
+                92.871,
+                0.513
+            ],
+            "output": [
+                0
+            ]
         }
+    ],
+    "break": "break",
+    "run": [
+        0.0912,
+        0.798,
+        219947,
+        0.675,
+        0,
+        0.0894,
+        0.0442,
+        101.956,
+        0.842
+    ]
 }
 var str = ""
 var obj;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 exports.handler = function (event, context, callback) {
-    var data = {}
-    const s = JSON.stringify(event.body);
-    console.log(s)
-    // for (let i = 1; i < s.length - 1; i++) {
-    //     const e = s[i].split('\\')
-    //     // console.log(e)
-    //     // data[e[2].replace('"', '')] = Number(e[3].replace('": ', '').replace(',', ''))
-    // }
-    // console.log(data)
+    const synaptic = require('synaptic'); // this line is not needed in the browser
+    const Neuron = synaptic.Neuron,
+        Layer = synaptic.Layer,
+        Network = synaptic.Network,
+        Trainer = synaptic.Trainer,
+        Architect = synaptic.Architect;
+    const a = JSON.stringify(event.body).split("\\").join('').split(',"break":"break",').join();
+    const b = a.slice(1, a.length - 1)
+    const data = JSON.parse(b)
+    const x = data
+    console.log(x)
+
+
+    const net = new Architect.Perceptron(9, 7, 6, 1)
+    const trainer = new Trainer(net)
+    var trainingSet = data.train
+    trainer.train(trainingSet)
+    var aa = net.activate(data.run)
+
     callback(null, {
         statusCode: 200,
         headers: {
@@ -16,7 +26,12 @@ exports.handler = function (event, context, callback) {
             'Access-Control-Allow-Headers':
                 'Origin, X-Requested-With, Content-Type, Accept',
         },
-        body: s//JSON.stringify({ msg: "Hello", other: 3456, bd: event.body })
+        body: JSON.stringify(aa[0])
     })
 
 }
+
+{/*
+
+{"train":[{"input":[0.00321,0.451,226336,0.661,0,0.108,0.0976,180.133,0.444],"output":[1]},{"input":[0.0934,0.423,216563,0.661,0.000129,0.125,0.07,114.26,0.621,1],"output":[1]},{"input":[0.0133,0.52,166733,0.878,0,0.0433,0.0616,168.033,0.783],"output":[1]},{"input":[0.0157,0.663,212613,0.777,0.00829,0.0755,0.0375,143.003,0.94],"output":[1]},{"input":[0.00146,0.514,200040,0.73,9.54e-5,0.0897,0.0598,171.005,0.334],"output":[1]},{"input":[0.0912,0.798,219947,0.675,0,0.0894,0.0442,101.956,0.842],"output":[0]},{"input":[0.00205,0.57,178600,0.865,0,0.0404,0.0472,110.01,0.691],"output":[0]},{"input":[0.167,0.491,189187,0.867,0,0.458,0.103,167.965,0.602],"output":[1]},{"input":[0.634,0.554,218497,0.776,0.00277,0.115,0.0722,169.052,0.324],"output":[1]},{"input":[0.0774,0.649,282987,0.587,0,0.377,0.316,92.871,0.513],"output":[0]}],"break":"break","run":[0.0912,0.798,219947,0.675,0,0.0894,0.0442,101.956,0.842]}
+*/}
