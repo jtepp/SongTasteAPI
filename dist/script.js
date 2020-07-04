cb1fc2e5c1a944b89b41b9aff50da99ce4879325
Jacob Tepperman <jtepp@icloud.com>
2020-07-03 23:38:31 -0400

    added touch event for close dataview

--- dist/script.js
+++ dist/script.js
@@ -118,6 +118,11 @@ else if (window.location.href.includes('app.html')) {
             v.setAttribute('style', 'top: 3000px')
         }
     })
+    document.addEventListener('touchstart', (event) => {
+        if (v.getAttribute('style') == 'top: 50px' && !event.path.includes(v) && !event.path.includes(document.getElementById('viewdatabutton'))) {
+            v.setAttribute('style', 'top: 3000px')
+        }
+    })
     asyncApp()
 
 }
