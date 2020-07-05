464a47cb1c8de86b012d051064ef6873888e3b02
Jacob Tepperman <jtepp@icloud.com>
2020-07-05 17:02:51 -0400

    playlsist create function

--- dist/plscript.js
+++ dist/plscript.js
@@ -1,6 +1,13 @@
 const creds = "NGRjZDczOTlmNDk1NGUyYzhjNjc5ZjM4ZDFiYjE0MTk6ZWNmOWExODVjYzZjNDI4NmJkMjA3NTNhMThmZTVmYzU=";
 const err = new URLSearchParams(window.location.search).get('error') == null
 const code = new URLSearchParams(window.location.search).get('code')
+const playlistJSON = {
+    "name": 'SongTaste Favorites',
+    "public": true,
+    "collaborative": false,
+    "description": "A professionally curated playlist from SongTaste.cf, and its neural network."
+
+}
 var tokenObj = {}
 // if (err) window.location.href = 'https://songtaste.netlify.app/app'
 // else {
@@ -16,6 +23,8 @@ run()
 async function run() {
     tokenObj = await token()
     console.log(tokenObj)
+    await createPlaylist(playlistJSON)
+
 }
 
 
@@ -30,4 +39,16 @@ async function token() {
         body: `grant_type=authorization_code&code=${code}&redirect_uri=https://songtaste.netlify.app/app:playlist`
     }).then(res => res.json())
 
+}
+
+async function createPlaylist(bodyJSON) {
+    const user_id = 'me'
+    await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/JSON',
+            'Authorization': `Basic ${creds}`
+        },
+        body: bodyJSON
+    })
 }
