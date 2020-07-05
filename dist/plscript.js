988000d55aadc94f6ca34245a72f12c389395879
Jacob Tepperman <jtepp@icloud.com>
2020-07-05 15:40:52 -0400

    show error

--- dist/plscript.js
+++ dist/plscript.js
@@ -1,15 +1,15 @@
 const creds = "NGRjZDczOTlmNDk1NGUyYzhjNjc5ZjM4ZDFiYjE0MTk6ZWNmOWExODVjYzZjNDI4NmJkMjA3NTNhMThmZTVmYzU=";
 const err = new URLSearchParams(window.location.search).get('error') == null
 const code = new URLSearchParams(window.location.search).get('code')
-if (err) window.location.href = 'https://songtaste.netlify.app/app'
-else {
+// if (err) window.location.href = 'https://songtaste.netlify.app/app'
+// else {
 
-    token()
-
-
+token()
 
 
-}
+
+
+// }
 
 async function token() {
     fetch("https://accounts.spotify.com/api/token", {
