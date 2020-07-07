1a82a624486732766908b95780e9f90497169448
Jacob Tepperman <jtepp@icloud.com>
2020-07-07 15:52:43 -0400

    failure

--- src/app.js
+++ src/app.js
@@ -19,11 +19,17 @@ exports.handler = function (event, context, callback) {
 
 
         // const net = new Architect.Perceptron(9, 6, 1)
-        const net = (data.transfernet == {} || data.transfernet == null) ? new Architect.Perceptron(9, 7, 6, 1) : data.transfernet
-        console.log("net: " + JSON.stringify(net))
-        const trainer = (data.transfertrainer == {} || data.transfertrainer == null) ? new Trainer(net) : data.transfertrainer
-        console.log("trainer: " + JSON.stringify(trainer))
-        trainer.train(data.Atrain)
+        try {
+            const net = (data.transfernet == {} || data.transfernet == null) ? new Architect.Perceptron(9, 7, 6, 1) : data.transfernet
+            console.log("net: " + JSON.stringify(net))
+            const trainer = (data.transfertrainer == {} || data.transfertrainer == null) ? new Trainer(net) : data.transfertrainer
+            console.log("trainer: " + JSON.stringify(trainer))
+            trainer.train(data.Atrain)
+        } catch (err) {
+            console.log('im a failure ' + err)
+            const net = new Architect.Perceptron(9, 7, 6, 1)
+            const trainer = new Trainer(net)
+        }
         //propagate last result
         try {
             net.activate(data.Atrain[data.Atrain.length - 1].input)
