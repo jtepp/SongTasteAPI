dffcb5079bd970c8ba023b63ee3df83c2cf5e31f
Jacob Tepperman <jtepp@icloud.com>
2020-07-07 19:33:41 -0400

    maintain longth

--- dist/script.js
+++ dist/script.js
@@ -123,7 +123,7 @@ if (window.location.href.includes('index.html') || window.location.pathname == '
     retrieveSong(randomWord(wordLength), homePreview[2], '2')
 }
 else if (window.location.href.includes('/app')) {
-
+    document.getElementById('longth').value = window.localStorage.getItem('length') || '5'
 
     updateTEXT()
 
